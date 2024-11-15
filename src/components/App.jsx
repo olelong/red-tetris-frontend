e9c3335b17b8e21b85d2ff1fba2cf6e125cccf12
olelong <olelong@student.42.fr>
2024-11-15 20:51:55 +0100

    Trying to connect with sockets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,51 @@
-// import { useSelector, useDispatch } from "react-redux";
-// import { updateUsername, updateRoomName, reset, createRoom } from "../store/store";
+import { useEffect, useState } from "react";
 import "../styles/App.css";
 import Board from "./Board.jsx";
+import { socket } from "../socket.js";
 
 export default function App() {
-  // const room = useSelector((state) => state.string.room);
-  // const username = useSelector((state) => state.string.username);
-  // const dispatch = useDispatch();
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    // function onDisconnect() {
+    //   setIsConnected(false);
+    // }
+
+    socket.on("connect", onConnect);
+    console.log(socket?.connected, isConnected, socket);
+    // socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      // socket.off("disconnect", onDisconnect);
+    };
+
+    //   socket.on("game:update", (data) => {
+    //     console.log("game update:", data);
+    //   });
+  }, []);
 
   return (
     <div>
       <h1>Red Tetris</h1>
       <Board />
+    </div>
+  );
+}
 
-      {/* <h2>
+// import { useSelector, useDispatch } from "react-redux";
+// import { updateUsername, updateRoomName, reset, createRoom } from "../store/store";
+
+// const room = useSelector((state) => state.string.room);
+// const username = useSelector((state) => state.string.username);
+// const dispatch = useDispatch();
+
+{
+  /* <h2>
         Room : {room}, username: {username}
       </h2>
       <button
@@ -28,7 +60,5 @@ export default function App() {
 
       <button onClick={() => dispatch(reset())}>Reset</button>
 
-      <button onClick={() => dispatch(createRoom())}>create a room</button> */}
-    </div>
-  );
+      <button onClick={() => dispatch(createRoom())}>create a room</button> */
 }

c376feae5a872149923c0c07fb71eab52fb58778
olelong <olelong@student.42.fr>
2024-11-29 02:19:07 +0100

    Adding fruit themes and fairy theme

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,43 +2,67 @@ import { useState, useEffect } from "react";
 import { socket } from "../socket.js";
 
 import "../styles/Board.css";
-import redCat from "../assets/catsTheme/red-cat.png";
-import blueCat from "../assets/catsTheme/blue-cat.png";
-import lightBlueCat from "../assets/catsTheme/light-blue-cat.png";
-import greenCat from "../assets/catsTheme/green-cat.png";
-import orangeCat from "../assets/catsTheme/orange-cat.png";
-import purpleCat from "../assets/catsTheme/purple-cat.png";
-import yellowCat from "../assets/catsTheme/yellow-cat.png";
-import penaltyCat from "../assets/catsTheme/penalty-cat.png";
+import catRed from "../assets/catsTheme/red-cat.png";
+import catBlue from "../assets/catsTheme/blue-cat.png";
+import catLightBlue from "../assets/catsTheme/light-blue-cat.png";
+import catGreen from "../assets/catsTheme/green-cat.png";
+import catOrange from "../assets/catsTheme/orange-cat.png";
+import catPurple from "../assets/catsTheme/purple-cat.png";
+import catYellow from "../assets/catsTheme/yellow-cat.png";
+import catPenalty from "../assets/catsTheme/penalty-cat.png";
 
-// import fairyGreen from "../assets/fairiesTheme/fairyGreen.png";
+import fairyGreen from "../assets/fairiesTheme/fairyGreen.png";
+import fairyBlue from "../assets/fairiesTheme/fairyBlue.png";
+import fairyCyan from "../assets/fairiesTheme/fairyCyan.png";
+import fairyOrange from "../assets/fairiesTheme/fairyOrange.png";
+import fairyPurple from "../assets/fairiesTheme/fairyPurple.png";
+import fairyRed from "../assets/fairiesTheme/fairyRed.png";
+import fairyYellow from "../assets/fairiesTheme/fairyYellow.png";
+import fairyPenalty from "../assets/fairiesTheme/fairyPenalty.png";
 
+import fruitKiwi from "../assets/fruitsTheme/kiwi.png";
+import fruitStrawberry from "../assets/fruitsTheme/strawberry.png";
+import fruitBaies from "../assets/fruitsTheme/baies.png";
+import fruitFigue from "../assets/fruitsTheme/figue.png";
+import fruitLemon from "../assets/fruitsTheme/lemon.png";
+import fruitMyrtille from "../assets/fruitsTheme/myrtille.png";
+import fruitOrange from "../assets/fruitsTheme/orange.png";
+import fruitPenalty from "../assets/fruitsTheme/durian.png";
 
-// import kiwi from "../assets/fruitsTheme/kiwi.png";
-// import strawberry from "../assets/fruitsTheme/strawberry.png";
-
-// const fruitThemeMap = {
-//   0: "none", // Empty
-// 1: `url(${})`, // Cyan
-// 2: `url(${})`, // Blue
-// 3: `url(${})`, // Orange
-// 4: `url(${})`, // Yellow
-// 5: `url(${kiwi})`, // Green
-// 6: `url(${})`, // Purple
-// 7: `url(${strawberry})`, // Red
-// 8: `url(${})`, // Penalty grey
-// };
+const fruitsThemeMap = {
+  0: "none", // Empty
+  1: `url(${fruitBaies})`, // Cyan
+  2: `url(${fruitMyrtille})`, // Blue
+  3: `url(${fruitOrange})`, // Orange
+  4: `url(${fruitLemon})`, // Yellow
+  5: `url(${fruitKiwi})`, // Green
+  6: `url(${fruitFigue})`, // Purple
+  7: `url(${fruitStrawberry})`, // Red
+  8: `url(${fruitPenalty})`, // Penalty grey
+};
 
 const catThemeMap = {
   0: "none", // Empty
-  1: `url(${lightBlueCat})`, // Cyan
-  2: `url(${blueCat})`, // Blue
-  3: `url(${orangeCat})`, // Orange
-  4: `url(${yellowCat})`, // Yellow
-  5: `url(${greenCat})`, // Green
-  6: `url(${purpleCat})`, // Purple
-  7: `url(${redCat})`, // Red
-  8: `url(${penaltyCat})`, // Penalty grey
+  1: `url(${catLightBlue})`, // Cyan
+  2: `url(${catBlue})`, // Blue
+  3: `url(${catOrange})`, // Orange
+  4: `url(${catYellow})`, // Yellow
+  5: `url(${catGreen})`, // Green
+  6: `url(${catPurple})`, // Purple
+  7: `url(${catRed})`, // Red
+  8: `url(${catPenalty})`, // Penalty grey
+};
+
+const fairiesThemeMap = {
+  0: "none", // Empty
+  1: `url(${fairyCyan})`, // Cyan
+  2: `url(${fairyBlue})`, // Blue
+  3: `url(${fairyOrange})`, // Orange
+  4: `url(${fairyYellow})`, // Yellow
+  5: `url(${fairyGreen})`, // Green
+  6: `url(${fairyPurple})`, // Purple
+  7: `url(${fairyRed})`, // Red
+  8: `url(${fairyPenalty})`, // Penalty grey
 };
 
 export default function Board() {
@@ -66,7 +90,7 @@ export default function Board() {
             className="grid-item"
             style={{
               backgroundColor: num === 8 ? "#cdd7e0" : "#ffffff",
-              backgroundImage: catThemeMap[num],
+              backgroundImage: fruitsThemeMap[num], //catThemeMap[num],
               backgroundSize: "cover",
             }}
           />
